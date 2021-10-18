c6e6f2474e065b3e508c809f45f9aa06368d32e8
Thomas Vigneron <thomas.vigneron@outlook.com>
2021-10-18 14:14:32 +0200

    Finalisation du projet

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ sequence[0]=0;      // initialisation de la case 0 du tableau à 0 pour indiquer
  function on_off(idTouche,etat) {
     if(etat==1)
      switch(idTouche) {
-     case 1: document.getElementById('couleur1').style.backgroundColor = "#ff0000";
-    break;
-     case 2: document.getElementById('couleur2').style.backgroundColor = "#00ff00";
-    break;
-     case 3: document.getElementById('couleur3').style.backgroundColor = "#0000ff";
-    break;
-     case 4: document.getElementById('couleur4').style.backgroundColor = "#ffff00";
-    break;
+        case 1: document.getElementById('couleur1').style.backgroundColor = "#ff0000";
+        break;
+        case 2: document.getElementById('couleur2').style.backgroundColor = "#00ff00";
+        break;
+        case 3: document.getElementById('couleur3').style.backgroundColor = "#0000ff";
+        break;
+        case 4: document.getElementById('couleur4').style.backgroundColor = "#ffff00";
+        break;
      }
      else {
      document.getElementById('couleur1').style.backgroundColor = "#5f0000";
@@ -34,4 +34,55 @@ sequence[0]=0;      // initialisation de la case 0 du tableau à 0 pour indiquer
      document.getElementById('couleur3').style.backgroundColor = "#00005f";
      document.getElementById('couleur4').style.backgroundColor = "#5f5f00";
      }
+}
+
+function readClicOnTouche(idTouche) {
+    if(sequence[0]==0) {
+        document.getElementById('message').value = "C'est partie !";
+        initSequence();
+        }
+        else {
+        if(idTouche == sequence[compteur]) {
+            compteur ++;
+            if(compteur>compteurMax) initSequence();
+        }
+        else {
+            document.getElementById('message').value = "Erreur !! Appuyer sur une couleur pour recommencer";
+            sequence[0]=0;
+            compteur=0;
+        }
     }
+       
+}
+
+function initSequence() {
+    compteurMax = compteur;
+    compteur = 0;
+    sequence[compteurMax] = getNbreAleatoire();
+    animSequence();
+}
+
+function getNbreAleatoire(){
+    var nbreAleatoire;
+    do {
+    nbreAleatoire = Math.floor((Math.random() * 10) + 1);
+    }while(nbreAleatoire>4);
+    return nbreAleatoire;
+}
+
+function animSequence() {
+    idAnimation = setTimeout('animSequence();', 500);
+    if(etatOnOff==0) {
+        on_off(sequence[compteur],1);
+        etatOnOff=1;
+    }
+    else {
+        on_off(sequence[compteur],0);
+        etatOnOff=0;
+        compteur++;
+        if(compteur>compteurMax){
+            clearTimeout(idAnimation);
+            compteur=0;
+        }
+    } 
+}
